51321706967a8e99cf70d6b8b59a5f90f18b0d94
Ayomide Bajo <oluwashinabajo@gmail.com>
2021-08-11 19:45:58 +0100

    Add configurations for production

--- app.js
+++ app.js
@@ -3,12 +3,11 @@ const express = require("express");
 const path = require("path");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
-const compression = require("compression");
 const helmet = require("helmet");
-
 const indexRouter = require("./routes/index");
 const usersRouter = require("./routes/users");
 const catalogRouter = require("./routes/catalog");
+const compressor = require("compression");
 
 const app = express();
 
@@ -16,7 +15,10 @@ const app = express();
 const mongoose = require("mongoose");
 const mongoDB =
   "mongodb+srv://localLibraryUser:password2020@locallibrarynode.dgqa9.mongodb.net/localLibraryUser?retryWrites=true&w=majority";
-mongoose.connect(mongoDB, { userNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect(process.env.MONGODB_URI || mongoDB, {
+  userNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
 const db = mongoose.connection;
 
@@ -31,13 +33,11 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
-app.use(helmet());
+app.use(compressor());
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
 app.use("/catalog", catalogRouter);
-
-app.use(compression());
-
+app.use(helmet());
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
   next(createError(404));
@@ -47,7 +47,7 @@ app.use(function (req, res, next) {
 app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get("env") === "production" ? err : {};
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
   // render the error page
   res.status(err.status || 500);
